769ee21b1cc3366ced08748f06ed80da3d203c41
Jaeyoung Lee <jaeyoung@monodiary.net>
2020-04-16 14:17:17 +0900

    Apply qolplus/core

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/QOLPlus/discord-bot
 go 1.14
 
 require (
+	github.com/QOLPlus/core v1.0.0
 	github.com/bwmarrin/discordgo v0.20.3
 	github.com/cosmtrek/air v1.12.1 // indirect
+	github.com/dustin/go-humanize v1.0.0
 	github.com/fatih/color v1.9.0 // indirect
 	github.com/imdario/mergo v0.3.9 // indirect
 	github.com/k0kubun/pp v3.0.1+incompatible
--- handlers/stock/process.go
+++ handlers/stock/process.go
@@ -3,10 +3,123 @@ package stock
 import (
 	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"github.com/dustin/go-humanize"
+	"strings"
+
+	"github.com/QOLPlus/core/commands/stock"
 )
 
-const Command = "주식"
+const (
+	Command = "주식"
+	MaxCount = 3
+)
 
 func Process(s *discordgo.Session, m *discordgo.MessageCreate, data []string) {
-	fmt.Println("잘못들어옴", data)
+	codes := make([]string, 0)
+
+	for _, keyword := range data {
+		searchAssetsResult, err := stock.SearchAssetsByKeyword(keyword)
+		if err != nil {
+			continue
+		}
+
+		searchCodes := searchAssetsResult.GetCodes()
+		if len(searchCodes) == 0 {
+			continue
+		}
+		codes = append(codes, searchCodes[0:1]...)
+	}
+
+	if len(codes) == 0 {
+		_, _ = s.ChannelMessageSend(m.ChannelID, "주식 정보를 찾을 수 없습니다.")
+		return
+	}
+
+	fetchSecuritiesResult, err := stock.FetchSecuritiesByCodes(codes)
+	if err != nil {
+		_, _ = s.ChannelMessageSend(m.ChannelID, "주식 정보를 불러올 수 없습니다.")
+		return
+	}
+
+	for idx, security := range fetchSecuritiesResult.RecentSecurities {
+		_, _ = s.ChannelMessageSendEmbed(
+			m.ChannelID,
+			buildEmbedFromSecurity(security),
+		)
+		if idx == MaxCount {
+			warning := fmt.Sprintf("한번에 %d종목 이상을 조회할 수 없습니다.", MaxCount)
+			_, _ = s.ChannelMessageSend(m.ChannelID, warning)
+		}
+	}
+}
+
+func buildEmbedFromSecurity(security stock.FetchedSecurity) *discordgo.MessageEmbed {
+	var (
+		embedArrow string
+		embedColor int
+		embedMarket string
+	)
+
+	switch security.Change {
+	case "UPPER_LIMIT":
+		embedArrow = "↑"
+		embedColor = 16062488
+	case "RISE":
+		embedArrow = "▲"
+		embedColor = 16062488
+	case "FALL":
+		embedArrow = "▼"
+		embedColor = 1794513
+	case "LOWER_LIMIT":
+		embedArrow = "↓"
+		embedColor = 1794513
+	default:
+		embedArrow = ""
+		embedColor = 0
+	}
+
+	if security.Market == "KOSDAQ" {
+		embedMarket = "*"
+	} else {
+		embedMarket = ""
+	}
+
+	return &discordgo.MessageEmbed{
+		Color: embedColor,
+		Thumbnail: &discordgo.MessageEmbedThumbnail{
+			URL: "https://fn-chart.dunamu.com/images/kr/stock/d/mini/" + security.ShortCode + ".png",
+		},
+		Fields: []*discordgo.MessageEmbedField{
+			{
+				Name: fmt.Sprintf(
+					"%s%s",
+					security.Name,
+					embedMarket,
+				),
+				Value: strings.Join([]string{
+					humanize.Commaf(security.TradePrice),
+					fmt.Sprintf("%s%.2f", embedArrow, security.ChangePrice),
+					"(" + fmt.Sprintf("%.2f", security.ChangePriceRate * 100) + "%)",
+				}, " "),
+			},
+			{
+				Name: "시가 / 종가",
+				Value: fmt.Sprintf("%s / %s", humanize.Commaf(security.OpeningPrice), humanize.Commaf(security.PrevClosingPrice)),
+				Inline: true,
+			},
+			{
+				Name: "고가 / 저가",
+				Value: fmt.Sprintf("%s / %s", humanize.Commaf(security.HighPrice), humanize.Commaf(security.LowPrice)),
+				Inline: true,
+			},
+			{
+				Name: "거래량",
+				Value: fmt.Sprintf(
+					"%s 주 (%s 원)",
+					humanize.Comma(security.AccTradeVolume),
+					humanize.Commaf(security.GlobalAccTradePrice),
+				),
+			},
+		},
+	}
 }
