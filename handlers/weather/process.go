42d964e92f47ddc779e2f9508eec449247d24e81
Jaeyoung Lee <jaeyoung@monodiary.net>
2020-04-26 20:26:00 +0900

    Add weather command

--- handlers/weather/process.go
+++ handlers/weather/process.go
@@ -2,21 +2,48 @@ package weather
 
 import (
 	"fmt"
+	"github.com/QOLPlus/core/commands/weather"
+	"github.com/QOLPlus/discord-bot/refs"
 	"github.com/bwmarrin/discordgo"
 	"strings"
-
-	"github.com/QOLPlus/discord-bot/refs"
 )
 
 var Registry = &refs.HandlerRegistry{
-	Commands: []string{"날씨", "웨더"},
+	Commands: []string{"날씨", "웨더", "날씨"},
 	Proc:     Process,
 }
 
 func Process(s *discordgo.Session, m *discordgo.MessageCreate, data []string) {
-	_, err := s.ChannelMessageSend(m.ChannelID, "날씨 기능은 아직 지원하지 않습니다. PR 환영\n요청한 지역:" + strings.Join(data, ","))
-	if err != nil {
-		fmt.Println("message 전송 실패!")
+	for _, keyword := range data {
+		if len(keyword) == 0 {
+			keyword = "반포동"
+		}
+		keyword = strings.ReplaceAll(keyword, " ", "")
+		data, err := weather.FetchLocationByKeyword(keyword)
+		if err != nil {
+			_, _ = s.ChannelMessageSend(m.ChannelID, "알 수 없는 에러가 발생했습니다.")
+			return
+		}
+
+		region := data.GetFirstRegionCode()
+		fetched, err := weather.FetchWeather(region)
+		if err != nil {
+			_, _ = s.ChannelMessageSend(m.ChannelID, "알 수 없는 에러가 발생했습니다.")
+		}
+		_, _ = s.ChannelMessageSend(m.ChannelID, buildMessage(fetched))
 	}
 }
 
+func buildMessage(r *weather.FetchWeatherResult) string {
+	return fmt.Sprintf(
+		"**%s**: %s **%d**°C (체감 %d°, 어제보다 %d°) [최저 %d° ~ 최고 %d°]",
+		r.Location,
+		r.Status,
+
+		int(r.Temperature),
+		int(r.TemperatureDayFeel),
+		int(r.DiffWithYesterday),
+		int(r.TemperatureDayLow),
+		int(r.TemperatureDayHigh),
+	)
+}
